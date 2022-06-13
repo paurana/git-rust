1ffb55f1982240bf265ce91fce6291d2b3cccb7a
paurana ubuntu <f20200803@pilani.bits-pilani.ac.in>
2022-06-13 06:07:23 +0530

    stage 2

---
+++ src/args.rs
@@ -0,0 +1,68 @@
+use flate2::read::ZlibDecoder;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn parse(args: Vec<String>) -> Result<()> {
+    let arg = &args[1][..];
+    match arg {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+            println!("Initialized git directory");
+            Ok(())
+        }
+        "cat-file" => cat_file(args),
+        _ => {
+            println!("unknown command: {}", args[1]);
+            Ok(())
+        }
+    }
+}
+
+pub fn cat_file(args: Vec<String>) -> Result<()> {
+    let option = &args[2][..];
+    match option {
+        "-p" => {
+            let object_name = &args[3];
+            let dir_sha = &object_name[..2];
+            let file_name = &object_name[2..];
+
+            let file_dir = format!(".git/objects/{}/{}", dir_sha, file_name);
+            let f = File::open(file_dir)?;
+
+            let mut z = ZlibDecoder::new(f);
+            let mut s = String::new();
+            z.read_to_string(&mut s)?;
+
+            // Git first constructs a header which starts by identifying the type of object --- in this case, a blob.
+            // To that first part of the header, Git adds a space followed by the size in bytes of the content, and adding a final null byte:
+            let s = &s[5..s.len() - 1];
+            //start_length = 5 to remove "blob "
+            //s.len()-1 to remove the null byte at the end
+
+            let mut counter = 0; // to keep track of i
+            for i in 0..s.len() {
+                if let Ok(length) = s[..i].parse::<usize>() {
+                    if length == s[i..].len() {
+                        counter = i;
+                        break;
+                    }
+                }
+            }
+
+            let content = &s[counter..];
+            println!("{}", content);
+            Ok(())
+        }
+        second_arg => {
+            println!("{second_arg} not supported with cat-file");
+            Ok(())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
-use std::fs;
+mod args;
 
-fn main() {
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
-    }
+    args::parse(args)
 }
